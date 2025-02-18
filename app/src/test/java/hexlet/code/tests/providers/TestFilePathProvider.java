5ed1a8290243e8e6b2625fff6aefabe8ab48cfbe
Anastasia Lukash <anastasia16rom@gmail.com>
2025-02-19 00:13:55 +0100

    add tests

--- app/src/test/java/hexlet/code/tests/providers/TestFilePathProvider.java
+++ app/src/test/java/hexlet/code/tests/providers/TestFilePathProvider.java
@@ -3,7 +3,8 @@ package hexlet.code.tests.providers;
 import java.nio.file.Paths;
 
 public class TestFilePathProvider {
+    private static final String TEST_DATA_PATH = "src/test/java/hexlet/resources";
     public static String getResourceFilePath(String fileName) {
-        return Paths.get("src/test/java/hexlet/resources", fileName).toString();
+        return Paths.get(TEST_DATA_PATH, fileName).toString();
     }
 }
