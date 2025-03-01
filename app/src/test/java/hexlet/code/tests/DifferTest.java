c14fe3c26faf2f3a935d47b4df8c382209e0f4aa
Anastasia Lukash <anastasia16rom@gmail.com>
2025-03-01 11:39:36 +0100

    add tests for nested files

--- app/src/test/java/hexlet/code/tests/DifferTest.java
+++ app/src/test/java/hexlet/code/tests/DifferTest.java
@@ -1,37 +1,53 @@
 package hexlet.code.tests;
 
 import hexlet.code.Differ;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
-import static hexlet.code.tests.providers.TestFilePathProvider.getResourceFilePath;
+import static hexlet.code.tests.providers.TestDataProvider.getResourceFilePath;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
     Logger logger = Logger.getLogger(DifferTest.class.getName());
     Differ differ = new Differ();
 
-    @Test
-    public void shouldGenerateExpectedJsonDiffWhenComparingTwoFiles() {
-        String firstPath = getResourceFilePath("simpleJsonFirst.json");
-        String secondPath = getResourceFilePath("simpleJsonSecond.json");
-
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void shouldGenerateExpectedDiffWhenComparingTwoFiles(String firstPath, String secondPath, String expectedDiff) {
         logger.info("Comparing two files");
         String methodResult = differ.generate(firstPath, secondPath);
-        assertEquals(getExpectedResultForSimpleFiles(), methodResult,
-                "The generated JSON diff did not match the expected output.");
+        assertEquals(expectedDiff, methodResult, "The generated diff did not match the expected output.");
     }
 
-    @Test
-    public void shouldGenerateExpectedYmlDiffWhenComparingTwoFiles() {
-        String firstPath = getResourceFilePath("simpleYmlFirst.yml");
-        String secondPath = getResourceFilePath("simpleYmlSecond.yml");
-
-        logger.info("Comparing two files");
-        String methodResult = differ.generate(firstPath, secondPath);
-        assertEquals(getExpectedResultForSimpleFiles(), methodResult,
-                "The generated JSON diff did not match the expected output.");
+    private static Stream<Arguments> dataProvider() {
+        String expectedSimpleFilesDiff = getExpectedResultForSimpleFiles();
+        String expectedNestedFilesDiff = getExpectedResultForNestedFiles();
+        return Stream.of(
+                Arguments.of(
+                        getResourceFilePath("simpleJsonFirst.json"), //todo: remove for simple once support for nested is implemented
+                        getResourceFilePath("simpleJsonSecond.json"),
+                        expectedSimpleFilesDiff
+                ),
+                Arguments.of(
+                        getResourceFilePath("simpleYmlFirst.yml"),
+                        getResourceFilePath("simpleYmlSecond.yml"),
+                        expectedSimpleFilesDiff
+                ),
+                Arguments.of(
+                        getResourceFilePath("nestedJsonFirst.json"),
+                        getResourceFilePath("nestedJsonSecond.json"),
+                        expectedNestedFilesDiff
+                ),
+                Arguments.of(
+                        getResourceFilePath("nestedYmlFirst.yml"),
+                        getResourceFilePath("nestedYmlSecond.yml"),
+                        expectedNestedFilesDiff
+                )
+        );
     }
 
     private static String getExpectedResultForSimpleFiles() {
@@ -44,5 +60,34 @@ class DifferTest {
                   + timeout: 20
                   + verbose: true
                 }""";
+    }
+
+    private static String getExpectedResultForNestedFiles() {
+        return """
+                {
+                     chars1: [a, b, c]
+                   - chars2: [d, e, f]
+                   + chars2: false
+                   - checked: false
+                   + checked: true
+                   - default: null
+                   + default: [value1, value2]
+                   - id: 45
+                   + id: null
+                   - key1: value1
+                   + key2: value2
+                     numbers1: [1, 2, 3, 4]
+                   - numbers2: [2, 3, 4, 5]
+                   + numbers2: [22, 33, 44, 55]
+                   - numbers3: [3, 4, 5]
+                   + numbers4: [4, 5, 6]
+                   + obj1: {nestedKey=value, isNested=true}
+                   - setting1: Some value
+                   + setting1: Another value
+                   - setting2: 200
+                   + setting2: 300
+                   - setting3: true
+                   + setting3: none
+                 }""";
     }
 }
--- app/src/test/java/hexlet/code/tests/providers/TestDataProvider.java
+++ app/src/test/java/hexlet/code/tests/providers/TestDataProvider.java
@@ -1,9 +1,12 @@
 package hexlet.code.tests.providers;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TestFilePathProvider {
+public class TestDataProvider {
     private static final String TEST_DATA_PATH = "src/test/java/hexlet/resources";
+
     public static String getResourceFilePath(String fileName) {
         return Paths.get(TEST_DATA_PATH, fileName).toString();
     }
