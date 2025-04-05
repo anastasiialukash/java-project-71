4853d960587162d31b9018704a21f2f47636b60c
Anastasia Lukash <anastasia16rom@gmail.com>
2025-04-06 01:25:28 +0200

    fix build

--- app/src/main/java/hexlet/code/Formatters.java
+++ app/src/main/java/hexlet/code/Formatters.java
@@ -34,21 +34,21 @@ public class Formatters {
         sb.append("{\n");
         sortedKeys.forEach(fieldName -> {
             if (diff.getRemovedItems().containsKey(fieldName)) {
-                sb.append(String.format("    - %s: %s%n", fieldName,
+                sb.append(String.format("  - %s: %s%n", fieldName,
                         nodeToSingleLine(diff.getRemovedItems().get(fieldName))));
             }
             if (diff.getChangedItems().containsKey(fieldName)) {
-                sb.append(String.format("    - %s: %s%n", fieldName,
+                sb.append(String.format("  - %s: %s%n", fieldName,
                         nodeToSingleLine(diff.getChangedItems().get(fieldName).oldValue())));
-                sb.append(String.format("    + %s: %s%n", fieldName,
+                sb.append(String.format("  + %s: %s%n", fieldName,
                         nodeToSingleLine(diff.getChangedItems().get(fieldName).newValue())));
             }
             if (diff.getUnchangedItems().containsKey(fieldName)) {
-                sb.append(String.format("      %s: %s%n", fieldName,
+                sb.append(String.format("    %s: %s%n", fieldName,
                         nodeToSingleLine(diff.getUnchangedItems().get(fieldName))));
             }
             if (diff.getAddedItems().containsKey(fieldName)) {
-                sb.append(String.format("    + %s: %s%n", fieldName,
+                sb.append(String.format("  + %s: %s%n", fieldName,
                         nodeToSingleLine(diff.getAddedItems().get(fieldName))));
             }
         });
