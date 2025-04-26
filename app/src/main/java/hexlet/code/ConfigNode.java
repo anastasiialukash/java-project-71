15d0369aa19ee0e36c1c86a604c71705120b799c
Anastasia Lukash <anastasia16rom@gmail.com>
2025-04-26 17:22:20 +0200

    add javadoc for ConfigNode.java

--- app/src/main/java/hexlet/code/ConfigNode.java
+++ app/src/main/java/hexlet/code/ConfigNode.java
@@ -3,41 +3,65 @@ package hexlet.code;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class ConfigNode {
-    String key;
-    NodeState state;
-    JsonNode oldValue;
-    JsonNode newValue;
+    private String key;
+    private NodeState state;
+    private JsonNode oldValue;
+    private JsonNode newValue;
 
+    /***
+     * Get key of a ConfigNode object
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /***
+     * Set key for a ConfigNode object
+     */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /***
+     * Get state of a ConfigNode object
+     */
     public NodeState getState() {
         return state;
     }
 
+    /***
+     * Set status for a ConfigNode object
+     */
     public void setState(NodeState state) {
         this.state = state;
     }
 
+    /***
+     * Get oldValue of a ConfigNode object
+     */
     public JsonNode getOldValue() {
         return oldValue;
     }
 
+    /***
+     * Set oldValue for a ConfigNode object
+     */
     public void setOldValue(JsonNode oldValue) {
         this.oldValue = oldValue;
     }
 
+    /***
+     * Get newValue of a ConfigNode object
+     */
     public JsonNode getNewValue() {
         return newValue;
     }
 
+    /***
+     * Set newValue for a ConfigNode object
+     */
     public void setNewValue(JsonNode newValue) {
         this.newValue = newValue;
-    }
-
-    public String getKey() {
-        return key;
     }
 
     ConfigNode(String key, NodeState state, JsonNode oldValue, JsonNode newValue) {
